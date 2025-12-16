8339ee5c764768031566795d2b774db5583bcf6a
Thomas <tberrios9800@gmail.com>
2025-12-16 21:18:25 +0000

    Update multiply assembly code to use umull

--- src/multiply.s
+++ src/multiply.s
@@ -3,5 +3,5 @@
     .type multiply, @function
 multiply:
     movl    %edi, %eax   # move first int arg (in %edi) into return reg %eax
-    mul     %esi        # multiply second int arg (in %esi) with the value in return registry
-    ret                   # return; caller continues
+    imull   %esi, %eax   # multiply second int arg (in %esi) with the value in return registry
+    ret                  # return; caller continues
